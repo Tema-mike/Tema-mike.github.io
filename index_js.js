fc465d16849ecbdb1265d36291b555dcadd36874
Artem K <k.artem.d@gmail.com>
2020-10-03 21:02:03 +0400

    Initial commit

--- index_js.js
+++ index_js.js
@@ -2,11 +2,11 @@
 
 $(".menu").hover(function(){
     $(this).addClass('action-menu');
-    $($(this).children).addClass('action-item');
-    //console.log($)
+    $((this).children).addClass('action-item');
+    console.log($((this).children).attr('class'));
 }, function () {
     $(this).removeClass('action-menu');
-    $($(this).children).removeClass('action-item');
+    $((this).children).removeClass('action-item');
 
 });
 
