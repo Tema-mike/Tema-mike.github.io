39c09344d4929004b26a34f2b389876d9bf6c220
Artem K <k.artem.d@gmail.com>
2020-10-03 17:26:41 +0400

    Initial commit

--- index_js.js
+++ index_js.js
@@ -1,11 +1,10 @@
 
 
 $(".menu").hover(e => {
-    let menu = $(".menu").hover.attr('id');
+    let menu = $(".menu").attr('id');
     //let id_menu = menu.attr('id');
 
     console.log (menu);
-    id_item = 0;
 
 });
 
