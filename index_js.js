b4cc978fa554eb3d249dad41fefae8b24f6dfd2d
Artem K <k.artem.d@gmail.com>
2020-10-03 20:44:06 +0400

    Initial commit

--- index_js.js
+++ index_js.js
@@ -3,10 +3,11 @@
 $(".menu").hover(function(){
     $(this).addClass('action-menu');
     $((this).children).addClass('action-item');
-
+    console.log('pidor');
 }, function () {
     $(this).removeClass('action-menu');
-    $((this).children).removeClass('action-item')
+    $((this).children).removeClass('action-item');
+    console.log('No pidor');
 });
 
 
