d053b6be3b6b75628f68710ddcb7d97cec3c76aa
Artem K <k.artem.d@gmail.com>
2020-10-03 17:46:51 +0400

    Initial commit

--- index_js.js
+++ index_js.js
@@ -2,12 +2,16 @@
 
 $(".menu").hover(e => function(){
 
+    $(this).addClass('action-menu');
     let menu = $(this).attr('id');
     //let id_menu = menu.attr('id');
 
     console.log (menu);
 
+}, function () {
+    $(this).removeClass('action-menu');
 });
+
 
 function itemAnimate() {
 
