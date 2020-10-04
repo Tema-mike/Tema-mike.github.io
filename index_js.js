e5d101a3e2a5ae7e19734b47ebdfec45d25b9ec2
Artem K <k.artem.d@gmail.com>
2020-10-04 18:27:34 +0400

    Initial commit

--- index_js.js
+++ index_js.js
@@ -1,17 +1,18 @@
-
+$(document).ready(() => {
+    setTimeout(preFont,1000);
+});
+function preFont(){
+    $(".is-preload").removeClass("is-preload");
+    $("h1").addClass("logo-text");
+}
 
 $(".menu").hover(function(){
     $(this).addClass('action-menu');
     $((this).children).addClass('action-item');
-    console.log($((this).children).attr('class'));
+    $((this).children).css("display", "block");
+
 }, function () {
     $(this).removeClass('action-menu');
     $((this).children).removeClass('action-item');
-
+    $((this).children).css("display", "none");
 });
-
-
-//function itemAnimate() {
-
-
-//}
