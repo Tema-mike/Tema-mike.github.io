8be4b35e1b68d7780032eb97e08ad4182927fc73
Artem K <k.artem.d@gmail.com>
2020-10-22 23:50:32 +0400

    Initial commit

--- index_js.js
+++ index_js.js
@@ -1,3 +1,4 @@
+// Start animation
 $(document).ready(() => {
     setTimeout(preFont,1000);
 });
@@ -6,6 +7,7 @@ function preFont(){
     $("h1").addClass("logo-text");
 }
 
+// animation of menu's items
 $(".menu").hover(function(){
     $(this).addClass('action-menu');
     $((this).children).addClass('action-item');
@@ -15,4 +17,14 @@ $(".menu").hover(function(){
     $(this).removeClass('action-menu');
     $((this).children).removeClass('action-item');
     $((this).children).css("display", "none");
+});
+
+// Mobile form for menu's items
+$("#menu-btn").click(event => {
+    $('body').css('overflow','hidden');
+    $("#menu-container").css('display','block');
+});
+$("#close").click(event => {
+    $('body').css('overflow','auto');
+    $("#menu-container").css('display','none');
 });
